443d356dc37abb3f77a7b4fcb58ee16ed7ea2ec9
premanathreddy21125a1207 <117589488+premanathreddy21125a1207@users.noreply.github.com>
2025-03-15 18:07:45 +0530

    Add files via upload

---
+++ ProjectdbContext.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Recipe.Models;
+
+namespace Recipe.Data;
+
+public partial class ProjectdbContext : DbContext
+{
+    public ProjectdbContext()
+    {
+    }
+
+    public ProjectdbContext(DbContextOptions<ProjectdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Ingredient> Ingredients { get; set; }
+
+    public virtual DbSet<Instruction> Instructions { get; set; }
+
+    //public virtual DbSet<Recipe> Recipes { get; set; }
+    public virtual DbSet<Recipe.Models.Recipe> Recipes { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=PREM\\SQLEXPRESS;integrated security =SSPI;database= projectdb ; TrustServerCertificate=true  ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ingredient>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Ingredie__3213E83F3941B4A9");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.Quantity)
+                .HasMaxLength(100)
+                .HasColumnName("quantity");
+            entity.Property(e => e.RecipeId).HasColumnName("recipe_id");
+
+            entity.HasOne(d => d.Recipe).WithMany(p => p.Ingredients)
+                .HasForeignKey(d => d.RecipeId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Ingredien__recip__3F466844");
+        });
+
+        modelBuilder.Entity<Instruction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Instruct__3213E83F1BF93938");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.RecipeId).HasColumnName("recipe_id");
+            entity.Property(e => e.StepDescription).HasColumnName("step_description");
+            entity.Property(e => e.StepNumber).HasColumnName("step_number");
+
+            entity.HasOne(d => d.Recipe).WithMany(p => p.Instructions)
+                .HasForeignKey(d => d.RecipeId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Instructi__recip__4222D4EF");
+        });
+
+        modelBuilder.Entity<Recipe.Models.Recipe>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Recipes__3213E83F996E1740");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Category)
+                .HasMaxLength(100)
+                .HasColumnName("category");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .HasColumnName("title");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Recipes)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__Recipes__user_id__3C69FB99");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3213E83FD3CE622A");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__AB6E6164CB960240").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .HasColumnName("email");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
